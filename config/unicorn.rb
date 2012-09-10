80996e2216190aec808dec133dd8d1c06b546753
mgawlista <mg@reqorder.com>
2012-09-10 20:51:27 +0200

    Renames application from blog to volontariat

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/blog/current"
+root = "/home/deployer/apps/volontariat/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.volontariat.sock"
 worker_processes 2
 timeout 30
