6f8a42c7045822eccc5152c9a21ca4a989f53c7a
mgawlista <mg@reqorder.com>
2012-09-22 12:42:29 +0200

    Adds step definition for a new vacancy with state

--- features/step_definitions/vacancy_steps.rb
+++ features/step_definitions/vacancy_steps.rb
@@ -1,10 +1,28 @@
+module VacancyFactoryMethods
+  def set_vacancy_defaults(attributes)
+    attributes[:user_id] ||= @me.id unless attributes[:user_id] || !@me
+    attributes[:project_id] ||= Project.last.id unless attributes[:project_id] || Project.all.none?
+  end
+  
+  def new_vacancy(name, state = nil)
+    attributes = { name: name }
+    attributes[:state] = state if state
+    
+    set_vacancy_defaults(attributes)
+    
+    @vacancy = Factory(:vacancy, attributes)
+    @vacancy.reload
+  end
+end
+
+World(VacancyFactoryMethods)
+
 Given /^a vacancy named "([^\"]*)"$/ do |name|
-  attributes = {name: name}
-  attributes[:project_id] ||= Project.last.id if Project.any?
-  
-  @vacancy = Factory(:vacancy, attributes)
-  
-  @vacancy.reload
+  new_vacancy(name)
+end
+
+Given /^a vacancy named "([^\"]*)" with state "([^\"]*)"$/ do |name, state|
+  new_vacancy(name, state)
 end
 
 When /^I delete the (\d+)(?:st|nd|rd|th) vacancy$/ do |pos|
