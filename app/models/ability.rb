4f6e3b06e9076c15f24f57be6ee74c72947b5a9a
mgawlista <mg@reqorder.com>
2012-09-24 16:18:56 +0200

    Tidy up.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,48 +14,29 @@ class Ability
     alias_action :restful_actions, :moderation_actions, to: :moderate
     alias_action :read, :assign_actions, to: :supervisor
     
-    can :read, Area
-    can :read, Project
-    can :read, Vacancy
-    can :read, Candidature
-    can :read, Comment
+    can :read, [
+      Area, Product, Project, Vacancy, Candidature, Story, Task, Result, Comment
+    ]
     can [:read, :check_name, :check_url, :check_email, :check_email_unblocked], User
     
     if user.present?
       can :destroy, User, id: user.id
       
-      can [:new, :create], Area
-      can [:new, :create], Project
-      can [:new, :create], Vacancy
-      can [:new, :create], Candidature
-      can [:new, :create], Comment
-      #can :create, [Like, Dislike]
+      can [:new, :create], [Area, Project, Vacancy, Candidature, Comment]
       
-      can :restful_actions, Project, user_id: user.id
-      can :restful_actions, Vacancy, offeror_id: user.id
-      can :restful_actions, Candidature, user_id: user.id
-      can :restful_actions, Comment, user_id: user.id
-      #can :restful_actions, [Like, Dislike], user_id: user.id
-      can :restful_actions, ProjectUser, user_id: user.id
+      { 
+        user_id: [Product, Project, Candidature, Comment, ProjectUser, Result], 
+        offeror_id: [Vacancy, Story, Task]
+      }.each do |attribute, classes|
+        can :restful_actions, classes, attribute => user.id   
+      end
       
       can Candidature::EVENTS, Candidature, offeror_id: user.id
       can Vacancy::EVENTS, Vacancy, offeror_id: user.id
       
-      #can :administrate, Area if user.roles.where('name = "Administrator"').any?
-
-      #can :moderate, Project, project_users: {user_id: user.id, role_id: Role.find('Administrator')}
-
-      #if user.master?
-      #  can [:manage, :moderate, :administrate, :supervisor], :all
-      #end
-      
-      if user.name == 'master'
+      if user.name == 'Master'
         can [:manage, :moderate, :administrate, :supervisor], :all
       end
-    end
-
-    if controller_namespace.starts_with?('admin')
-      cannot :manage, :all # unless
     end
   end
 end
