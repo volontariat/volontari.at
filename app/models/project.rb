86a891f4e8807fc2d8d1ab972958e06bdc2d6250
mgawlista <mg@reqorder.com>
2012-09-24 16:19:50 +0200

    Adds mongo db associations. Includes areas of product before validation.

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,12 +14,36 @@ class Project < ActiveRecord::Base
   validates :user_id, presence: true
   validates :name, presence: true, uniqueness: true
   validates :text, presence: true
+  validates :area_ids, presence: true
   
-  attr_accessible :name, :text, :url, :area_ids
+  attr_accessible :product_id, :name, :text, :url, :area_ids
   
   extend FriendlyId
   
   friendly_id :name, use: :slugged
   
+  before_validation :include_areas_of_product
+  
   PARENT_TYPES = ['area', 'user']
+  
+  # belongs_to (Mongo DB)
+  def product
+    product_id.blank? ? nil : Product.find(product_id)
+  end
+    
+  # has_many (Mongo DB)
+  def stories
+    Story.where(project_id: id)
+  end
+  
+  private
+  
+  def include_areas_of_product
+    self.area_ids ||= []
+    
+    if product
+      self.area_ids += product.areas.map(&:id)
+      self.area_ids.uniq!
+    end
+  end
 end
