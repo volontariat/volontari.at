149271e2c9759de195bc681991335c1ef29b7fbf
mgawlista <mg@reqorder.com>
2012-10-04 18:29:02 +0200

    Adds method render_product_specific_partial_if_available

---
+++ app/helpers/product_helper.rb
@@ -0,0 +1,22 @@
+module ProductHelper
+  def render_product_specific_partial_if_available(resource, partial_path, options = {})
+    path = nil
+    partial_path = partial_path.split('/')
+    file_name = partial_path.pop
+    partial_path = partial_path.join('/')
+    
+    if resource && resource.respond_to?(:product) && resource.product
+      path = "products/types/#{directory_for_resource(resource.product)}/#{partial_path}"
+      
+      if File.exist?("#{Rails.root.to_s}/app/views/#{path}/_#{file_name}.html.erb")
+        path = [path, file_name].join('/')
+      else
+        path = nil
+      end
+    end
+
+    path = [partial_path, file_name].join('/') if path.blank?
+
+    render path, options 
+  end
+end
