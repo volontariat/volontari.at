79888aac0a3257e0842a8f9ce3d76bba67d26958
mgawlista <mg@reqorder.com>
2012-10-04 18:19:43 +0200

    Adds method show_actions

--- app/helpers/show_helper.rb
+++ app/helpers/show_helper.rb
@@ -2,4 +2,15 @@ module ShowHelper
   def show_field(title, value)
     value.blank? ? '' : content_tag(:dt, title) + content_tag(:dd, value)
   end
+  
+  def show_actions
+    result  = content_tag :dt, raw('&nbsp')
+    result += content_tag :dd, render(
+      partial: 'shared/resource/actions', locals: { 
+        type: root_model_class_name(resource).tableize, resource: resource 
+      }
+    )
+    
+    result
+  end
 end
