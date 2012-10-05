2cc5c51a0e10dd0dc8d9ae1436f0103f06933ba6
mgawlista <mg@reqorder.com>
2012-10-05 16:34:18 +0200

    Get delete link working in action dropdowns

---
+++ app/assets/javascripts/functions.js
@@ -0,0 +1,24 @@
+function delete_link(id) {
+  link = $(id)
+  
+  if(!confirm(link.data('confirm'))) {
+    return
+  }
+  
+  var href = $.rails.href(link),
+    method = link.data('method'),
+    target = link.attr('target'),
+    csrf_token = $('meta[name=csrf-token]').attr('content'),
+    csrf_param = $('meta[name=csrf-param]').attr('content'),
+    form = $('<form method="post" action="' + href + '"></form>'),
+    metadata_input = '<input name="_method" value="' + method + '" type="hidden" />'
+
+  if (csrf_param !== undefined && csrf_token !== undefined) {
+    metadata_input += '<input name="' + csrf_param + '" value="' + csrf_token + '" type="hidden" />'
+  }
+
+  if (target) { form.attr('target', target) }
+
+  form.hide().append(metadata_input).appendTo('body')
+  form.submit()
+}
