1c0d7f6b720b46eb36355d0fdd796939711a3d9a
mgawlista <mg@reqorder.com>
2012-10-05 16:28:48 +0200

    Take first area or create a new one

--- spec/mongo_db_factories.rb
+++ spec/mongo_db_factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :product do
     name 'Text Creation'
     user_id User.first.try(:id) || Factory(:user).id
-    area_ids [Factory(:area).id]
+    area_ids [Area.first.try(:id) || Factory(:area).id]
     text Faker::Lorem.sentences(5)
     
     after_build do |product|
