b75c2feb4d357b5acd2ed6a1d67915bbf352d615
mgawlista <mg@reqorder.com>
2012-10-06 20:16:23 +0200

    Set database cleaner strategy to truncation

--- features/support/env.rb
+++ features/support/env.rb
@@ -40,7 +40,8 @@ ActionController::Base.allow_rescue = false
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
-  DatabaseCleaner.strategy = :transaction
+  DatabaseCleaner.strategy = :truncation
+  Cucumber::Rails::World.use_transactional_fixtures = false
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
