57700e87e88d396bc51c12b82c97dbbe30a3987a
mgawlista <mg@reqorder.com>
2012-10-06 18:10:43 +0200

    Adds new methods show_attributes, show_attribute, show_associations and show_association. Remove show_field method.

--- app/helpers/show_helper.rb
+++ app/helpers/show_helper.rb
@@ -1,6 +1,53 @@
 module ShowHelper
-  def show_field(title, value)
+  def show_attributes(*attributes)
+    result = ''
+    
+    attributes.each {|attribute| result += show_attribute attribute}
+    
+    raw result
+  end
+  
+  def show_attribute(attribute, options = {})
+    title = options[:title] || attribute_translation(attribute)
+    value = options[:value] || resource.send(attribute)
+    
     value.blank? ? '' : content_tag(:dt, title) + content_tag(:dd, value)
+  end
+  
+  def show_associations(*associations)
+    result = ''
+    
+    associations.each {|association| result += show_association association}
+    
+    raw result
+  end
+  
+  def show_association(association)
+    if association.to_s == association.to_s.pluralize
+      return show_attribute(
+        title: t("#{association}.index.title"), 
+        value: raw(resource.send(association).map{|a| link_to a.name, a}.join(', '))
+      )
+    end
+    
+    title = if general_attribute?(:parent)
+      t("activerecord.attributes.general.#{association}")
+    else
+      if resource.send(association) 
+        t("activerecord.models.#{root_model_class_name(resource.send(association)).underscore}")
+      else 
+        t("activerecord.models.#{association}")
+      end
+    end
+    
+    if resource.send(association)
+      show_attribute(
+        title: title, 
+        value: link_to(resource.send(association).try(:name), resource.send(association))
+      )  
+    else
+      ''
+    end
   end
   
   def show_actions
