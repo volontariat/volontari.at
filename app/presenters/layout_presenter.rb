9fdc36b8eeded28ed2d8d97514734376cfd5b411
mgawlista <mg@reqorder.com>
2012-10-23 19:57:59 +0200

    Adds Layout presenter to hold implemented table presenter

---
+++ app/presenters/layout_presenter.rb
@@ -0,0 +1,9 @@
+class LayoutPresenter < ResourcePresenter
+  def table_presenter(new_subject)
+    @table_presenter ||= Shared::Collection::TablePresenter.new(new_subject) 
+  end
+  
+  def table_presenter=(presenter)
+    @table_presenter = presenter 
+  end
+end
