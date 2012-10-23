5d1cedacbf191bda458bbf2b789cb56e06e2306b
mgawlista <mg@reqorder.com>
2012-10-23 19:55:56 +0200

    Replaces length by include? method checks.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,20 +7,44 @@ describe Product do
         I18n.locale = :de
         subject = Factory.build(:product, name: 'Text Creation')
         subject.valid?
-        subject.errors.messages[:name].length.should == 1
+        
+        subject.errors[:name].include?(
+           I18n.t(
+             'activerecord.errors.models.product.attributes.name.missing_english_name'
+           )
+        ).should == true
+        
         I18n.locale = I18n.default_locale
         subject = Factory.build(:product, name: 'Text Creation')
-        subject.valid?.should == true
+        subject.valid?
+        
+        subject.errors[:name].include?(
+           I18n.t(
+             'activerecord.errors.models.product.attributes.name.missing_english_name'
+           )
+        ).should == false
       end
     end
     
     describe 'existing_model_file?' do
-      it 'assures that a english name is present' do
+      it 'assures that a model file exists' do
         subject = Factory.build(:product, name: 'Unavailable')
         subject.valid?
-        subject.errors.messages[:name].length.should == 1
+     
+        subject.errors[:name].include?(
+          I18n.t(
+            'activerecord.errors.models.product.attributes.name.missing_model_file'
+          )
+        ).should == true
+        
         subject = Factory.build(:product, name: 'Text Creation')
         subject.valid?.should == true
+      
+        subject.errors[:name].include?(
+          I18n.t(
+            'activerecord.errors.models.product.attributes.name.missing_model_file'
+          )
+        ).should == false
       end
     end
   end
