469efb5041b74e389fdb05afd09903c30c6a4d62
mgawlista <mg@reqorder.com>
2012-10-23 19:45:28 +0200

    Removes add remove text workaround.

--- app/models/product/text_creation/task.rb
+++ app/models/product/text_creation/task.rb
@@ -1,25 +1,12 @@
 class Product::TextCreation::Task < ::Task
-  include StateMachines::Task
+  include StateMachines::Product::TextCreation::Task
   
   field :keywords, type: Array 
    
   attr_accessible :keywords
     
-  validates :text, presence: false
   validates :keywords, presence: true, unless: ->(t) { t.story.with_keywords.blank? }
   #validate :between_keyword_range
-
-  # WORKRAROUND: overriding text validation doesn't work
-  before_validation :add_text
-  after_validation :remove_text
   
-  private
-  
-  def add_text
-    self.text = 'Dummy'
-  end
-  
-  def remove_text
-    self.text = nil
-  end
+  #track_history on: [:user_id, :name, :text, :keywords, :state]
 end
