f4b8e53384e77e6851ef41609ff461b845c7da40
mgawlista <mg@reqorder.com>
2012-11-09 22:22:32 +0100

    Register recaptcha initializer

--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6Ld9SdcSAAAAADGzvuO07tqjImVkdbD8TvjpZBgx'
-  config.private_key = '6Ld9SdcSAAAAABL436FIpK5TLyrwIFZSEE_VhRHG'
-  config.proxy = 'http://volontari.at:8080'
-end
