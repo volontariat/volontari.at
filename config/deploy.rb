ae4c3458f2ed93343bc5f79135021953166e3fed
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-06-04 23:14:20 +0200

    Adds application.yml to git ignore list.

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,21 +30,25 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
+    put File.read("config/application.yml"), "#{shared_path}/config/application.yml"
     put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
     put File.read("config/email.yml"), "#{shared_path}/config/email.yml"
     put File.read("config/initializers/airbrake.rb"), "#{shared_path}/config/initializers/airbrake.rb"
     put File.read("config/initializers/recaptcha.rb"), "#{shared_path}/config/initializers/recaptcha.rb"
     put File.read("config/initializers/secret_token.rb"), "#{shared_path}/config/initializers/secret_token.rb"
   end
+  
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/email.yml #{release_path}/config/email.yml"
     run "ln -nfs #{shared_path}/config/initializers/airbrake.rb #{release_path}/config/initializers/airbrake.rb"
     run "ln -nfs #{shared_path}/config/initializers/recaptcha.rb #{release_path}/config/initializers/recaptcha.rb"
     run "ln -nfs #{shared_path}/config/initializers/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
   end
+  
   after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
@@ -55,8 +59,9 @@ namespace :deploy do
       exit
     end
   end
+  
   before "deploy", "deploy:check_revision"
 end
 
-        require './config/boot'
-        require 'airbrake/capistrano'
+require './config/boot'
+require 'airbrake/capistrano'
