8e84d48f0a3a02ce3fe98f6a5969c39a09309993
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-06-04 23:32:15 +0200

    Outsourced parts of the Gemfile to voluntary and voluntary product gems.

--- features/step_definitions/state_machines/vacancy_steps.rb
+++
@@ -1,7 +0,0 @@
-When /I click on the tab "([^"]*)"/ do |tab|
-  page.execute_script("$('a[href=\"##{tab.strip}\"]').click()")
-end
-
-When /I click on the (\d+)(?:st|nd|rd|th) link of a tab "([^"]*)"/ do |pos, tab|
-  find(:xpath, "id('#{tab}')//a[#{pos.to_i}]").click
-end
