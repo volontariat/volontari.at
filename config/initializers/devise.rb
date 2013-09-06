377430467785d2d984aec835383106e8482102d7
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-09-06 12:50:52 +0200

    Force require of devise through its initializer.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 
+require 'devise'
+
 class ActionController::Responder
   def to_mobile
     default_render
